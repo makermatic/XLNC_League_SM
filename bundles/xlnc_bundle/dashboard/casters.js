cfbb48c2851a3082efd00418cda4eebce51a93b3
makermatic <44180512+makermatic@users.noreply.github.com>
2021-09-06 13:35:47 -0400

    Dashboard rewrite
    Text inputs are now selects

--- bundles/xlnc_bundle/dashboard/casters.js
+++ bundles/xlnc_bundle/dashboard/casters.js
@@ -1,16 +1,17 @@
 const leftCaster = document.getElementById('leftCaster')
 const rightCaster = document.getElementById('rightCaster')
+const analyst = document.getElementById('analyst')
 
 var swapTog = !true;
 
 function update() {
-    const data = { leftCaster: leftCaster.value, rightCaster: rightCaster.value }
+    const data = { leftCaster: leftCaster.value, rightCaster: rightCaster.value, analyst: analyst.value}
     nodecg.sendMessage('updateCasters', data)
 }
 
 function swap() {
     if (swapTog != true) {
-        const data = { leftCaster: leftCaster.value, rightCaster: rightCaster.value }
+        const data = { leftCaster: leftCaster.value, rightCaster: rightCaster.value, analyst: analyst.value }
         nodecg.sendMessage('swapCasters', data)
         swapTog = true;
     } else {
