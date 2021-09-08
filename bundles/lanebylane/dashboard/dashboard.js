fc2b4272026a18d3f179afff0c332117a225026b
makermatic <44180512+makermatic@users.noreply.github.com>
2021-09-08 19:16:12 -0400

    Backend for Lane by Lane

--- bundles/lanebylane/dashboard/dashboard.js
+++ bundles/lanebylane/dashboard/dashboard.js
@@ -11,7 +11,8 @@ const player3 = document.getElementById('P3')
 const player4 = document.getElementById('P4')
 
 function updatePlayers() {
-
+    const data = { player1: player1.value, player2: player2.value, player3: player3.value, player4: player4.value }
+    nodecg.sendMessage('updatePlayers', data)
 }
 
 function updateQuestion() {
@@ -20,33 +21,40 @@ function updateQuestion() {
 }
 
 function addP1() {
-    p1Rep.value + 100
+    p1Rep.value = p1Rep.value + 100
 }
 
 function subtractP1() {
-    p1Rep.value - 100;
+    p1Rep.value = p1Rep.value - 100
 }
 
 function addP2() {
-    p2Rep.value + 100;
+    p2Rep.value = p2Rep.value + 100
 }
 
 function subtractP2() {
-    p2Rep.value - 100;
+    p2Rep.value = p2Rep.value - 100
 }
 
 function addP3() {
-    p3Rep.value + 100;
+    p3Rep.value = p3Rep.value + 100
 }
 
 function subtractP3() {
-    p3Rep.value - 100;
+    p3Rep.value = p3Rep.value - 100
 }
 
 function addP4() {
-    p4Rep.value + 100
+    p4Rep.value = p4Rep.value + 100
 }
 
 function subtractP4() {
-    p4Rep.value - 100;
+    p4Rep.value = p4Rep.value - 100
+}
+
+function resetScores() {
+    p1Rep.value = 0
+    p2Rep.value = 0
+    p3Rep.value = 0
+    p4Rep.value = 0
 }
--- bundles/lanebylane/graphics/MainLayout.js
+++ bundles/lanebylane/graphics/MainLayout.js
@@ -7,19 +7,26 @@ const player3 = document.getElementById('player3');
 const player4 = document.getElementById('player4');
 
 // Score Replicants
-const p1Rep = nodecg.Replicant('p1score');
+const p1Rep = nodecg.Replicant('p1Score');
 const p2Rep = nodecg.Replicant('p2Score');
 const p3Rep = nodecg.Replicant('p3Score');
 const p4Rep = nodecg.Replicant('p4Score');
 
 //  Score Vars
-const p1Score = document.getElementById('p1score');
+const p1Score = document.getElementById('p1Score');
 const p2Score = document.getElementById('p2Score');
 const p3Score = document.getElementById('p3Score');
 const p4Score = document.getElementById('p4Score');
 
 nodecg.listenFor('updateQuestion', (data) => {
     question.innerHTML = data.question
+})
+
+nodecg.listenFor('updatePlayers', (data) => {
+    player1.innerHTML = data.player1
+    player2.innerHTML = data.player2
+    player3.innerHTML = data.player3
+    player4.innerHTML = data.player4
 })
 
 p1Rep.on('change', (newVal) => {
